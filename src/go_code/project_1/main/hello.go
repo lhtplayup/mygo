89d95dde467fd7ccdfbebd9451ac60dda6cba6d8
haitao.liu <haitao.liu@xjsd.com>
2024-03-05 12:14:54 +0800

    dockerfile

--- src/go_code/project_1/main/hello.go
+++ src/go_code/project_1/main/hello.go
@@ -2,8 +2,32 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
 )
+
+// 获取本机网卡IP
+func getLocalIP() (ipv4 string, err error) {
+	// 获取所有网卡
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return
+	}
+	// 取第一个非lo的网卡IP
+	for _, addr := range addrs {
+		// 这个网络地址是IP地址: ipv4, ipv6
+		ipNet, isIpNet := addr.(*net.IPNet)
+		if isIpNet && !ipNet.IP.IsLoopback() {
+			// 跳过IPV6
+			if ipNet.IP.To4() != nil {
+				ipv4 = ipNet.IP.String() // 192.168.1.1
+				return
+			}
+		}
+	}
+
+	return
+}
 
 // HTTP服务端配置
 // 业务请求相应处理
@@ -14,8 +38,10 @@ func hello(res http.ResponseWriter, req *http.Request) {
 func main() {
 	//路由到指定站点位置跳转相关函数处理
 	fmt.Println("服务端启动了。。。。。")
+	ipv4, _ := getLocalIP()
+	fmt.Printf("ipv4 is <%s>", ipv4)
 	http.HandleFunc("/", hello)
-	err := http.ListenAndServe("127.0.0.1:8080", nil)
+	err := http.ListenAndServe(ipv4+":8080", nil)
 	if err != nil {
 		fmt.Println("启动监听失败,err:", err)
 		return
